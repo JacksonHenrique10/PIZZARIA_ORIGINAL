ce329e99534fda35712cb543a860ccfb9aa9529d
Jackson Henrique Aparecido dos Santos <jackson.santos@viaduto.corp>
2024-10-08 21:04:12 -0300

    original

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: 'https://localhost:3333'
-    baseURL: 'http://192.168.1.71:3333'
+    baseURL: 'http://169.254.158.3:3333'
 })
 
 export {api};
